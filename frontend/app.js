d70783167226c9b2428439c77a15cb47d9acb4bc
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-06-25 19:53:37 -0500

    feat: :sparkles: dashboard
    se agrego el dashboard
    
    Carlos Villafrades

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,17 @@
 import countries from "./components/countries.js";
 
-countries.agregarCountry();
-countries.showForm();
-countries.showRegistro();
+let btnPais = document.querySelector("#btnPais");
+btnPais.addEventListener("click", (e)=>{
+    document.querySelector(".contPrincipal").innerHTML =  `
+    <div class="containerForm">
+        <form id="formCountries">
+            <label for="">Ingrese el Nombre del pais a agregar</label>
+            <input type="text" name="name_country" required placeholder="Nombre del pais">
+            <button type="submit"> Agregar </button>
+        </form>
+	</div
+    `;
+    countries.agregarCountry();
+    countries.showForm();
+    countries.showRegistro();
+})
--- frontend/components/countries.js
+++ frontend/components/countries.js
@@ -109,7 +109,6 @@ export default {
            this.updateCountry();
             })
         });
-        
     },
 
     updateCountry(){
